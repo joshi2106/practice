7fdbce97bbb009248fa53db712aa96ac3bbb7b12
Syam Joshi Kalaga <syamjoshik@gmail.com>
2026-02-25 03:59:17 +0530

    Fix route decorator syntax and clean up code

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def welcome():
     return jsonify(message="You are welcome!")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000)
